94d7f1e4c002468c8dd7001d914557b53b95f799
Elizabeth Vega M <eb.vega.m@gmail.com>
2018-08-06 10:49:51 -0400

    version 0.0.2

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,23 @@ addList.addEventListener('click', () => {
   addList.classList.add('d-none');
 });
 
-close.addEventListener('click', () => {
+
+function newList() {
+  let listName = document.getElementById('listText').value;
+  first.innerHTML = `<div id="newList" class="listContainer">
+  <div id="listHeader">
+    <h6>${listName}</h6>
+    <i class="fas fa-ellipsis-h"></i>
+  </div>
+  <div id="listBody">
+    <button id="addTask" type="button" class="btn" onclick="addTask()"><i class="fas fa-plus"></i>Añada una tarjeta</button>
+  </div>
+</div>`
+  second.innerHTML = `<button id="addList" type="button" class="btn btn-light">
+  <i class="fas fa-plus"></i>Añadir una lista</button>`
+};
+
+function close() {
   inputList.classList.add('d-none');
   addList.classList.remove('d-none');
-});
+};
