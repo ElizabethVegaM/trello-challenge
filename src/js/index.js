b61d14a5cfbb22348d37bed20c2f56d01e889ede
Elizabeth Vega M <eb.vega.m@gmail.com>
2018-08-06 12:52:06 -0400

    version 0.0.5, con funcionalidad 100%

--- src/js/index.js
+++ src/js/index.js
@@ -5,48 +5,52 @@ addList.addEventListener('click', () => {
 
 function newList() {
   let listName = document.getElementById('listText').value;
-  if(listName.length > 1) {
-  first.innerHTML = `<div id="newList" class="listContainer">
+  if (listName.length > 1) {
+    first.innerHTML = `<div id="newList" class="listContainer">
     <div id="listHeader">
       <h6>${listName}</h6>
       <i class="fas fa-ellipsis-h"></i>
     </div>
     <div id="listBody">
-      <button id="addTask" type="button" class="btn" onclick="addTask()"><i class="fas fa-plus"></i>Añada una tarjeta</button>
+      <button id="addTaskBtn" type="button" class="btn" onclick="addTask()"><i class="fas fa-plus"></i>Añada una tarjeta</button>
       <div id="inputTask" class="d-none">
         <textarea name="" id="taskArea" cols="36" rows="3" placeholder="Introduzca un título para esta tarjeta..."></textarea>
         <button id="addBtn" type="button" class="btn" onclick="newTask()">Añadir tarjeta</button>
-        <button id="close" class="btn" onclick="closeCard()">
+        <button id="closeCardBtn" class="btn" onclick="closeCard()">
           <i class="fas fa-times"></i>
         </button>
       </div>
     </div>
   </div>`
-  second.innerHTML = `<button id="addList" type="button" class="btn btn-light">
+    second.innerHTML = `<button id="addList" type="button" class="btn btn-light">
   <i class="fas fa-plus"></i>Añadir una lista</button>`
   }
 };
 
-function close() { // no funciona
+function closeList() { // no funciona
   inputList.classList.add('d-none');
   addList.classList.remove('d-none');
 };
 
 function closeCard() {
-
+  inputTask.classList.add('d-none');
+  addTaskBtn.classList.remove('d-none');
 }
 
 function addTask() {
   inputTask.classList.remove('d-none');
-  addTask.classList.add('d-none'); // no funciona
+  addTaskBtn.classList.add('d-none'); // no funciona
 }
 
 function newTask() {
   let newTask = taskArea.value;
-  listBody.innerHTML = `<div>
-    <button class="btn taskName">${newTask}
-    <i class="fas fa-pen"></i>
-    </button>
-  </div>` + listBody.innerHTML;
-  inputTask.classList.add('d-none');
+  if (newTask.length > 1) {
+    listBody.innerHTML = `<div>
+      <button class="btn taskName">${newTask}
+        <i class="fas fa-pen"></i>
+      </button>
+    </div>` + listBody.innerHTML;
+    inputTask.classList.add('d-none');
+    addTaskBtn.classList.remove('d-none');
+  }
 }
